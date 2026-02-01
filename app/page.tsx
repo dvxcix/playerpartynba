d667b6f50d2c9b47a40b44f82cb7e7bd5f1d256b
dvxcix <dev.vick@protonmail.com>
2026-02-01 14:46:58 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,5 @@
-"use client";
+import OddsTable from "@/components/OddsTable";
 
-import { useEffect, useState } from "react";
-import { DataTable } from "@/components/DataTable";
-import { columns, OddsRow } from "@/components/columns";
-import { isTodayInUserTimezone } from "@/lib/time";
-
-export default function Dashboard() {
-  const [data, setData] = useState<OddsRow[]>([]);
-
-  useEffect(() => {
-    fetch("/api/odds/latest")
-      .then(res => res.json())
-      .then(res => {
-        const todayOnly = res.rows.filter((r: OddsRow) =>
-          isTodayInUserTimezone(r.commence_time)
-        );
-        setData(todayOnly);
-      });
-  }, []);
-
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">
-        NBA Alternate Player Props (Today)
-      </h1>
-
-      <DataTable columns={columns} data={data} />
-    </main>
-  );
+export default function Page() {
+  return <OddsTable />;
 }
